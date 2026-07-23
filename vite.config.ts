0adbc66667c38768213a4d83f277056af20ec79e
Juan Pablo Fernandez <jpprogrammer1@gmail.com>
2026-07-23 13:34:52 -0300

    Update allowedHosts configuration in Vite to accept all hosts for preview environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["team-manager-production-a9d8.up.railway.app"]
+    allowedHosts: true
   },
   server: {
     watch: {
